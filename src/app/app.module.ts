2220bc9d1cba7585c7b90e82746beda144790ad4
qwweerr <robertzhan92@hotmail.com>
2020-08-17 13:16:13 -0400

    setup environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AudioplayerComponent } from './audioplayer/audioplayer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AudioplayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
