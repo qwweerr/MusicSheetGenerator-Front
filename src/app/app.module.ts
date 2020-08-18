fa2c6ab7aeaf5b6798ebd403ca840069c71692fc
qwweerr <robertzhan92@hotmail.com>
2020-08-18 15:18:21 -0400

    flx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AudioplayerComponent } from './audioplayer/audioplayer.component';
 import { AudiouploadComponent } from './audioupload/audioupload.component';
-import { DashboardComponent } from './dashboard/dashboard.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -19,6 +22,9 @@ import { DashboardComponent } from './dashboard/dashboard.component'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
