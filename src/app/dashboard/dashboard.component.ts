0ee4a7efd7795598d48185eba9793b28e9b6ce72
qwweerr <robertzhan92@hotmail.com>
2020-08-19 12:34:36 -0400

    single select

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,16 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MatSelectionList, MatSelectionListChange, MatListOption } from '@angular/material/list';
+import { MatSelectionList, MatSelectionListChange } from '@angular/material/list';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
   typesOfInstruments: string[] = ['Violin', 'Paino', 'Guitar'];
 
-  @ViewChild(MatSelectionList) instruments: MatSelectionList;
+  @ViewChild(MatSelectionList, {static: true}) instruments: MatSelectionList;
 
   ngOnInit(){
       this.instruments.selectionChange.subscribe((i: MatSelectionListChange) => {          
