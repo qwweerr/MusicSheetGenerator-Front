9d8a836f56ca5615a293313c588cf9debaf6d353
qwweerr <robertzhan92@hotmail.com>
2020-09-09 16:05:13 -0400

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'MusicGeneratorFront-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MusicGeneratorFront-app');
+    expect(app.data.title).toEqual('MusicGeneratorFront-app');
   });
 
   it('should render title', () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AudioplayerComponent } from './audioplayer/audioplayer.component';
-import { AudiouploadComponent } from './audioupload/audioupload.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -17,9 +14,6 @@ import {MatListModule} from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
-    AudioplayerComponent,
-    AudiouploadComponent,
-    DashboardComponent
   ],
   imports: [
     BrowserModule,
